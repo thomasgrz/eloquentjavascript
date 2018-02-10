d3225c98099ac1e9bbf5bb1a36487fbaee02348a
thomasgrz <35355900+thomasgrz@users.noreply.github.com>
2018-02-10 14:10:52 -0500

    Update deepEqual.js

--- deepEqual.js
+++ deepEqual.js
@@ -1,5 +1,18 @@
 function deepEqual(a,b){
-  if((typeof a!=='object'&&)()){
-  
-  }
+        if((typeof a==='object'&&a!==null)&&(typeof b==='object'&&b!==null)){
+                let a_keys = Object.keys(a)
+                let b_keys = Object.keys(b)
+                //compare length of the array of both objects
+                if(a_keys.length!==b_keys.length){return false}
+                for(let key in a_keys){
+                        if(a[key]!==b[key]){
+                                return false
+                        }
+                //compare them and only return a result if its false
+                        if(!deepEqual(a[key],b[key])){return false}
+                }
+        }else{
+                if(a!==b){return false}
+        }
+return true
 }
